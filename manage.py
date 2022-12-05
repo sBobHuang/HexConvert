cb30f344c39ca2f3c262ee0b675180ae92ecdaf1
BobHuang <30999153+sBobHuang@users.noreply.github.com>
2022-12-05 21:44:04 +0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -3,14 +3,10 @@
 
 from app import create_app
 from flask_script import Manager, Shell
-from flask_cors import CORS
 
 app = create_app('default')
-CORS(app, resources=r'/*')
-
 
 manager = Manager(app)
-
 
 def make_shell_context():
     return dict(app=app, db=db)
