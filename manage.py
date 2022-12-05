769d96f09d8a7253ce19d1052cafaaefc951afb0
BobHuang <30999153+sBobHuang@users.noreply.github.com>
2022-12-05 21:43:31 +0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -3,7 +3,6 @@
 
 from app import create_app
 from flask_script import Manager, Shell
-from flask_migrate import MigrateCommand, Migrate
 from flask_cors import CORS
 
 app = create_app('default')
@@ -11,15 +10,12 @@ CORS(app, resources=r'/*')
 
 
 manager = Manager(app)
-migrate = Migrate(app, db)
 
 
 def make_shell_context():
     return dict(app=app, db=db)
 
-
 manager.add_command("shell", Shell(make_context=make_shell_context))
-manager.add_command('db', MigrateCommand)
 
 if __name__ == "__main__":
     manager.run()
